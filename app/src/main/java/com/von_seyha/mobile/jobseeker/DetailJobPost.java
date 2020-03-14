ca21f579bf06e9f44bf15ad5f45bdbe521fd21a2
Dell <sreyreachsem0911@gmail.com>
2020-03-14 20:48:28 +0700

    commit

--- app/src/main/java/com/von_seyha/mobile/jobseeker/DetailJobPost.java
+++ app/src/main/java/com/von_seyha/mobile/jobseeker/DetailJobPost.java
@@ -33,38 +33,38 @@ public class DetailJobPost extends AppCompatActivity {
             }
         });
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId()){
-                    case R.id.nav_profile:
-//                        startActivity(new Intent(getApplicationContext(),activity_viewtype_job.class));
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId()){
+//                    case R.id.nav_profile:
+////                        startActivity(new Intent(getApplicationContext(),activity_viewtype_job.class));
+////                        overridePendingTransition(0,0);
+//                        return  true;
+//
+//                    case R.id.nav_job:
+//                        Intent typeJob = new Intent(getApplicationContext(),activity_viewtype_job.class);
+//                        startActivity(typeJob);
 //                        overridePendingTransition(0,0);
-                        return  true;
-
-                    case R.id.nav_job:
-                        Intent typeJob = new Intent(getApplicationContext(),activity_viewtype_job.class);
-                        startActivity(typeJob);
-                        overridePendingTransition(0,0);
-                        return  true;
-
-                    case R.id.nav_home:
-                        Intent typeCv = new Intent(getApplicationContext(),grid_viewitemhome.class);
-                        startActivity(typeCv);
-                        overridePendingTransition(0,0);
-                        return  true;
-
-                    case R.id.nav_cv:
-                        Intent intent3 = new Intent(getApplicationContext(),activity_viewtype_cv.class);
-                        startActivity(intent3);
-                        overridePendingTransition(0,0);
-                        return  true;
-                }
-
-                return false;
-            }
-        });
+//                        return  true;
+//
+//                    case R.id.nav_home:
+//                        Intent typeCv = new Intent(getApplicationContext(),grid_viewitemhome.class);
+//                        startActivity(typeCv);
+//                        overridePendingTransition(0,0);
+//                        return  true;
+//
+//                    case R.id.nav_cv:
+//                        Intent intent3 = new Intent(getApplicationContext(),activity_viewtype_cv.class);
+//                        startActivity(intent3);
+//                        overridePendingTransition(0,0);
+//                        return  true;
+//                }
+//
+//                return false;
+//            }
+//        });
 
 
         btn_Setting.setOnClickListener(new View.OnClickListener() {
